b18f2b16ad5d609783a635b21e383d9e0fd4e1c2
Chris Maddalena <chris.maddalena@gmail.com>
2016-01-30 13:24:37 -0500

    Improved reporting output

--- lib/domain_tools.py
+++ lib/domain_tools.py
@@ -4,6 +4,9 @@
 import os
 import shodan
 import whois
+from BeautifulSoup import BeautifulSoup
+import urllib2
+from colors import red, green
 
 # Try to get the user's Shodan API key
 try:
@@ -16,81 +19,124 @@ except:
     sho_api = None
 
 # Number of commands
-total = 5 # Tests
-
+total = 6 # Tests
 def collect(client,domain):
-	print """Viper will now attempt to gather information from DNS records and other sources.
-	"""
-    
-	client = client
-	domain = domain
-	# Create drectory for client reports
-	if not os.path.exists("reports/%s" % client):
+    print green("""Viper will now attempt to gather information from DNS records and other sources.""")
+
+    client = client
+    domain = domain
+    browser = urllib2.build_opener()
+    browser.addheaders = [('User-agent', 'Mozilla/5.0')] # Google-friendly user-agent
+	# Create drectory for client reports and report
+    if not os.path.exists("reports/%s" % client):
 		try:
 			os.mkdir("reports/%s" % client)
 		except:
-			print "[!] Could not create reports directory!"
+			print red("[!] Could not create reports directory!")
 
-    # Create the Domain Report
-	os.system('echo "### Domain Report for %s ###\n" >> reports/%s/Domain_Report.txt' % (domain,client))
-	print "[+] Running whois (1/%s)" % total
-    # Run whois
-	os.system('echo "\n---WHOIS Results---\n" >> reports/%s/Domain_Report.txt' % client)
-	who = whois.whois(domain)
-	os.system('echo "Registrant: %s" >> reports/%s/Domain_Report.txt' % (who.name,client))
-	os.system('echo "Organization: %s" >> reports/%s/Domain_Report.txt' % (who.org,client))
-	os.system('echo "Emails: %s" >> reports/%s/Domain_Report.txt' % (who.emails,client))
-	os.system('echo "Address: %s, %s %s, %s, %s" >> reports/%s/Domain_Report.txt' % (who.address,who.city,who.zipcode,who.state,who.country,client))
-	os.system('echo "DNS: %s" >> reports/%s/Domain_Report.txt' % (who.name_servers,client))
-	os.system('echo "DNSSEC: %s" >> reports/%s/Domain_Report.txt' % (who.dnssec,client))
-	os.system('echo "Status: %s" >> reports/%s/Domain_Report.txt' % (who.status,client))
+    file = 'reports/%s/Domain_Report.txt' % client
 
-    # Run urlcrazy
-	print "[+] Running urlcrazy (2/%s)" % total
-	os.system('echo "\n---URLCRAZY Results---\n" >> reports/%s/Domain_Report.txt' % client)
-	os.system('urlcrazy %s >> reports/%s/Domain_Report.txt' % (domain,domain))
+    with open(file, 'w') as report:
+        # Create the Domain Report
+    	report.write("### Domain Report for %s ###\n" % (domain))
+    	print green("[+] Running whois (1/%s)" % total)
+        # Run whois
+    	report.write("\n---WHOIS Results---\n")
+    	who = whois.whois(domain)
+    	report.write("Registrant: %s\n" % who.name)
+    	report.write("Organization: %s\n" % (who.org))
+        for email in who.emails:
+           report.write("Email: %s\n" % email)
+    	report.write("Address: %s, %s %s, %s, %s\n" % (who.address,who.city,who.zipcode,who.state,who.country))
+        for server in who.name_servers:
+            report.write("DNS: %s\n" % server)
+    	report.write("DNSSEC: %s\n" % (who.dnssec))
+    	report.write("Status: %s\n" % (who.status))
 
-    # Run dnsrecon for several different lookups
-	print "[+] Running dnsrecon (3/%s)" % total
-	os.system('echo "\n---DNSRECON Results---\n" >> reports/%s/Domain_Report.txt' % client)
-	# Standard lookup for records
-	os.system('dnsrecon -d %s -t std >> reports/%s/Domain_Report.txt' % (domain,domain))
-	# Google for sub-domains
-	os.system('dnsrecon -d %s -t goo >> reports/%s/Domain_Report.txt' % (domain,domain))
-	# Zone Transfers
-	os.system('dnsrecon -d %s -t axfr >> reports/%s/Domain_Report.txt' % (domain,domain))
-	# Sub-domains
-	os.system('dnsrecon -d %s -t brt -D /pentest/intelligence-gathering/dnsrecon/namelist.txt --iw -f >> reports/%s/Domain_Report.txt' % (domain,domain))
+        # Run urlcrazy
+    	print green("[+] Running urlcrazy (2/%s)" % total)
+    	report.write("\n---URLCRAZY Results---\n")
+    	os.system('urlcrazy %s >> reports/%s/Domain_Report.txt' % (domain,domain))
 
-    # Run firece
-	print "[+] Running fierce (4/%s)" % total
-	os.system('echo "\n---FIERCE Results---\n" >> reports/%s/Domain_Report.txt' % client)
-	os.system('fierce -dns %s -wordlist /pentest/intelligence-gathering/fierce/hosts.txt -suppress>> reports/%s/Domain_Report.txt' % (domain,client))
+        # Run dnsrecon for several different lookups
+    	print green("[+] Running dnsrecon (3/%s)" % total)
+    	report.write("\n---DNSRECON Results---\n")
+    	# Standard lookup for records
+    	os.system('dnsrecon -d %s -t std >> reports/%s/Domain_Report.txt' % (domain,domain))
+    	# Google for sub-domains
+    	os.system('dnsrecon -d %s -t goo >> reports/%s/Domain_Report.txt' % (domain,domain))
+    	# Zone Transfers
+    	os.system('dnsrecon -d %s -t axfr >> reports/%s/Domain_Report.txt' % (domain,domain))
+    	# Sub-domains
+    	os.system('dnsrecon -d %s -t brt -D /pentest/intelligence-gathering/dnsrecon/namelist.txt --iw -f >> reports/%s/Domain_Report.txt' % (domain,domain))
 
-    # Perform Shodan searches
-	print "[+] Checking Shodan (5/%s)" % total
-	api = shodan.Shodan(SHODAN_API_KEY)
-	os.system('echo "\n---SHODAN Results---\n" >> reports/%s/Domain_Report.txt' % client)
-	try:
-		# Use API key to search Shodan for client name and client domain
-		clientResults = api.search(client)
-		domainResults = api.search(domain)
+        # Run firece
+    	print green("[+] Running fierce (4/%s)" % total)
+    	report.write("\n---FIERCE Results---\n")
+    	os.system('fierce -dns %s -wordlist /pentest/intelligence-gathering/fierce/hosts.txt -suppress>> reports/%s/Domain_Report.txt' % (domain,client))
 
-		os.system('echo "Client name results found: %s" >> reports/%s/Domain_Report.txt' % (clientResults['total'],client))
-        # Pull the most interesting information from search results
-		for result in clientResults['matches']:
-				os.system('echo "IP: %s" >> reports/%s/Domain_Report.txt' % (result['ip_str'],client))
-				os.system('echo "Hostname: %s" >> reports/%s/Domain_Report.txt' % (result['hostnames'],client))
-				os.system('echo "OS: %s" >> reports/%s/Domain_Report.txt' % (result['os'],client))
-				os.system('echo "Port: %s" >> reports/%s/Domain_Report.txt' % (result['port'],client))
-				os.system('echo "Data: %s" >> reports/%s/Domain_Report.txt' % (result['data'],client))
-		os.system('echo "Domain results found: %s" >> reports/%s/Domain_Report.txt' % (domainResults['total'],client))
-		for result in domainResults['matches']:
-				os.system('echo "IP: %s" >> reports/%s/Domain_Report.txt' % (result['ip_str'],client))
-				os.system('echo "Hostname: %s" >> reports/%s/Domain_Report.txt' % (result['hostnames'],client))
-				os.system('echo "OS: %s" >> reports/%s/Domain_Report.txt' % (result['os'],client))
-				os.system('echo "Port: %s" >> reports/%s/Domain_Report.txt' % (result['port'],client))
-				os.system('echo "Data: %s \n" >> reports/%s/Domain_Report.txt' % (result['data'],client))
-	except shodan.APIError, e:
-			print 'Error: %s' % e
-			pass
+        # Perform Shodan searches
+    	print green("[+] Checking Shodan (5/%s)" % total)
+    	api = shodan.Shodan(SHODAN_API_KEY)
+    	report.write("\n---SHODAN Results---\n")
+    	try:
+    		# Use API key to search Shodan for client name and client domain
+    		clientResults = api.search(client)
+    		domainResults = api.search(domain)
+
+    		report.write("Client name results found: %s\n" % clientResults['total'])
+            # Pull the most interesting information from search results
+    		for result in clientResults['matches']:
+    				report.write("IP: %s\n" % result['ip_str'])
+    				report.write("Hostname: %s\n" % result['hostnames'])
+    				report.write("OS: %s\n" % result['os'])
+    				report.write("Port: %s\n" % result['port'])
+    				report.write("Data: %s\n" % result['data'])
+    		report.write("Domain results found: %s\n" % domainResults['total'])
+    		for result in domainResults['matches']:
+    				report.write("IP: %s\n" % result['ip_str'])
+    				report.write("Hostname: %s\n" % result['hostnames'])
+    				report.write("OS: %s\n" % result['os'])
+    				report.write("Port: %s\n" % result['port'])
+    				report.write("Data: %s\n" % result['data'])
+    	except shodan.APIError, e:
+    			print 'Error: %s' % e
+    			pass
+
+    	# Search for different login/logon/admin/administrator pages
+    	report.write("\n--- GOOGLE HACKING LOGIN Results ---\n")
+        print green("[+] Checking Google for login pages (6/%s)" % total)
+    	# Login
+    	for start in range(0,10):
+    		url = "https://www.google.com/search?q=site:%s+intitle:login&start=" % domain + str(start*10)
+    		page = browser.open(url)
+    		soup = BeautifulSoup(page)
+
+    		for cite in soup.findAll('cite'):
+    			report.write("%s\n" % cite.text)
+    	# Logon
+    	for start in range(0,10):
+    		url = "https://www.google.com/search?q=site:%s+intitle:logon&start=" % domain + str(start*10)
+    		page = browser.open(url)
+    		soup = BeautifulSoup(page)
+
+    		for cite in soup.findAll('cite'):
+    			report.write("%s\n" % cite.text)
+    	# Admin
+    	for start in range(0,10):
+    		url = "https://www.google.com/search?q=site:%s+intitle:admin&start=" % domain + str(start*10)
+    		page = browser.open(url)
+    		soup = BeautifulSoup(page)
+
+    		for cite in soup.findAll('cite'):
+    			report.write("%s\n" % cite.text)
+    	# Administrator
+    	for start in range(0,10):
+    		url = "https://www.google.com/search?q=site:%s+intitle:administrator&start=" % domain + str(start*10)
+    		page = browser.open(url)
+    		soup = BeautifulSoup(page)
+
+    		for cite in soup.findAll('cite'):
+    			report.write("%s\n" % cite.text)
+
+    report.close()
