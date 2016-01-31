e3734b5f285c8bf19e58aa1563d76affdc284932
Chris Maddalena <chris.maddalena@gmail.com>
2016-01-31 13:46:09 -0500

    Mass can implementation

--- lib/scan_tools.py
+++ lib/scan_tools.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import socket
 import nmap
+import time
+from zapv2 import ZAPv2
+from pprint import pprint
 from colors import red, green
 
 # Run nmap scans - it accepts the type of scan from pentestMenu()
@@ -25,7 +29,7 @@ def runNMAP(type):
 			print green("[+] Equiviliant command if nmap was run manually:")
 			print red(scanner.command_line())
 			for host in scanner.all_hosts():
-				print('Host: %s (%s)' % (host, scanner[host].hostname()))
+				print('\nHost: %s (%s)' % (host, scanner[host].hostname()))
 				print('State: %s' % scanner[host].state())
 				for proto in scanner[host].all_protocols():
 					print('----------')
@@ -55,3 +59,24 @@ def retBanner(ip, port):
 		return banner
 	except:
 		return
+
+def runMasscan(type):
+	scanType = type
+	infile = raw_input("Name of IP file: ")
+	outfile = raw_input("Name for output: ")
+
+	with open(infile,'r') as ips:
+		for ip in ips:
+			if scanType == 1:
+				print green("[+] Running full port scan with masscan")
+				command = "masscan -p0-65535 --rate 50000 --banners %s -oX %s" % (ip.rstrip(),outfile)
+				os.system(command)
+			if scanType == 2:
+				print green("""This option requires a configuration file for masscan. Provide yours if you have one. If you don't, go back.
+If you've never setup one, you can dump a configuration using this command:""")
+				print red("masscan -p80,8000-8100 10.0.0.0/8 --echo > xxx.conf")
+				conf = raw_input("Name of Masscan conf file: ")
+				print green("[+] Running masscan with %s" % conf)
+				command = "masscan -p0-65535 --rate 50000 --banners %s -oX %s" % (ip.rstrip(),outfile)
+				os.system(command)
+ 
