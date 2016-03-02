8b3248ed8b799ceebcb93df3a65b5a3a07ad991a
Chris Maddalena <chris.maddalena@gmail.com>
2016-03-02 17:43:02 -0500

    Bug fix

--- lib/phish_tools.py
+++ lib/phish_tools.py
@@ -8,7 +8,7 @@ from colors import red, green
 def parseName(file):
 	outputFile = "ParsedNames.txt"
 	# Read all of the names line by line
-	with open(file) as f:
+	with open(file,"r") as f:
 		print green("[+] Parsing names and outputting to %s" % outputFile)
 		names = [line.rstrip('\n').split(" ") for line in f]
 		output = open(outputFile,"w")
